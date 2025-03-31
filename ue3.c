a8ba47e5ed74c6b3f02f44dae56e8369a818901a
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-03-31 13:56:27 +0200

    variations with mix, step and smoothstep

--- ue3.c
+++ ue3.c
@@ -45,7 +45,9 @@ void innit(void){
         "uniform vec3 fragColor1;\n" // erste fragColor
         "uniform vec3 fragColor2;\n" // zweite fragColor
         "void main() {\n"
-        "    vec3 finalColor = mix(fragColor1, fragColor2, fragValue);\n" // erstellt die richtig Farbe mit übergang
+        //"    vec3 finalColor = mix(fragColor1, fragColor2, step( 0.5f, fragValue));\n" // erstellt die richtig Farbe mit übergang
+        "    vec3 finalColor = mix(fragColor1, fragColor2, smoothstep( 0.0f, 1.0f, fragValue));\n" // erstellt die richtig Farbe mit übergang
+        //    vec3 finalColor = mix(fragColor1, fragColor2, fragValue);\n" // erstellt die richtig Farbe mit übergang
         "    gl_FragColor = vec4(finalColor, 1.0);\n"  // Setzt die Farbe des Pixels
         "}\n"; 
     // Fragment Shader erstellen
